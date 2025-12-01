ee8da2c4ba1ef5ede72d24bf081ad28d3f193e49
Quierounombre <vicengandrade@gmail.com>
2025-12-01 22:54:11 +0100

    Cleaning

--- p3/script.sh
+++ p3/script.sh
@@ -15,15 +15,13 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f "$YAML_FILE"
 
-kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=800s
+kubectl wait --for=condition=Ready pod -n argocd --all --timeout=8000s
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
-echo "PASSWORD = " -n
+echo "PASSWORD---------------------------------"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-echo "\n"
-
-argocd cluster add dev
+echo "-----------------------------------------\n"
 
 echo "Deployment complete."
 
