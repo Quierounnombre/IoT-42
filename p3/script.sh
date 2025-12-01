07d4ace66838009872793def0730622bddd4b78c
Quierounombre <vicengandrade@gmail.com>
2025-12-01 22:47:33 +0100

    argocd login

--- p3/script.sh
+++ p3/script.sh
@@ -2,8 +2,8 @@
 
 YAML_FILE="config.yaml"
 
-k3d cluster delete prueba
-k3d cluster create prueba
+k3d cluster delete clusty
+k3d cluster create clusty
 
 BOLD="\033[1m"
 GREEN="\033[32m"
@@ -13,9 +13,17 @@ RESET="\033[0m"
 # Install Argo CD in kubectl
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl wait --for=condition=ready --timeout=600s pod --all -n argocd
+kubectl apply -f "$YAML_FILE"
+
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=800s
+
 kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
-kubectl apply -f "$YAML_FILE"
+echo "PASSWORD = " -n
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+echo "\n"
+
+argocd cluster add dev
+
 echo "Deployment complete."
 
