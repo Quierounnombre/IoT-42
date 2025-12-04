b203d645801af32709920fe1750c14de8f8ef21b
Quierounombre <vicengandrade@gmail.com>
2025-12-04 13:30:24 +0100

    update for argocd stability

--- p3/script.sh
+++ p3/script.sh
@@ -13,15 +13,29 @@ RESET="\033[0m"
 # Install Argo CD in kubectl
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 kubectl apply -f "$YAML_FILE"
 
-kubectl wait --for=condition=Ready pod -n argocd --all --timeout=8000s
+kubectl rollout status deployment -n argocd --timeout=800s
+kubectl rollout status deployment -n dev --timeout=800s
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+kubectl port-forward svc/dev-net -n dev 8888:80 &
 
 echo "PASSWORD---------------------------------"
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-echo "-----------------------------------------\n"
+ARGOCD_PSWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo "\n-----------------------------------------\n"
+
+kubectl config set-context --current --namespace=argocd
+argocd login localhost:8080 --username admin --password "$ARGOCD_PSWD" --insecure
+argocd app create will-app \
+  --repo https://github.com/Quierounnombre/IoT-42.git \
+  --path manifests \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace dev \
+  --revision master \
+  --sync-policy automated
+
 
 echo "Deployment complete."
 
